7900ab3bb794e7d95b2dd51da746fbb120c2e6c4
Adhiraj Pandey <pandey.adhiraj02@gmail.com>
2023-10-23 01:44:48 +0530

    Change in Updating Flow

--- tracker.py
+++ tracker.py
@@ -13,10 +13,8 @@ NOTION_API_TOKEN = os.getenv("NOTION_API_TOKEN")
 DEPLOYMENT_DB_ID = os.getenv("DEPLOYMENT_DB_ID")
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
-DEPLOYMENT_DB_OUTPUT_DATA_FILE = "deployment_db_raw_data.json"
 
-
-def get_current_datetime():
+def get_current_datetime() -> str:
     ist = pytz.timezone('Asia/Calcutta')
     current_datetime = datetime.now(ist)
 
@@ -25,31 +23,16 @@ def get_current_datetime():
     return formatted_datetime
 
 
-def send_message_to_discord(username, message):
-    try:
-        headers = {
-            "Content-Type": "application/json"
-        }
-        payload = {
-            "content": message,
-            "username": username,
-        }
-
-        response = requests.post(url=DISCORD_WEBHOOK_URL, data=json.dumps(payload), headers=headers)
-
-        return response.text
-
-    except Exception as e:
-        logging.error(e)
-        return None
+def set_logging_config():
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        handlers=[
+                            logging.FileHandler('deployment-tracker.log', encoding='utf-8'),
+                            logging.StreamHandler()
+                        ])
 
 
-def write_data_to_json_file(data, filename):
-    with open(filename, 'a', encoding='utf8') as file:
-        json.dump(data, file, indent=4)
-
-
-def read_notion_deployments_database(database_id):
+def read_notion_deployments_database(database_id: str) -> dict:
     try:
         headers = {
             "Authorization": "Bearer " + NOTION_API_TOKEN,
@@ -66,10 +49,9 @@ def read_notion_deployments_database(database_id):
 
     except Exception as e:
         logging.error(e)
-        return None
 
 
-def extract_data_from_notion_response(response_data):
+def extract_data_from_notion_response(response_data: dict) -> list[dict]:
     results = response_data["results"]
 
     extracted_data = []
@@ -78,7 +60,7 @@ def extract_data_from_notion_response(response_data):
         properties = result["properties"]
         extracted_data.append({
             "name": properties["Project Name"]["title"][0]["plain_text"],
-            "label": properties["Label"]["status"]["name"],
+            "status": properties["Label"]["status"]["name"],
             "link": properties["Link"]["rich_text"][0]["plain_text"],
             "notion_page_id": get_pageid_from_notion_url(result["url"]),
         })
@@ -86,91 +68,26 @@ def extract_data_from_notion_response(response_data):
     return extracted_data
 
 
-def check_project_deployment_status(project_link, max_retries=3, timeout=5):
+def check_project_deployment_status(project_link: str, max_retries: int = 5, timeout: int = 5) -> str:
     try:
         for i in range(max_retries):
             response = requests.get(project_link, allow_redirects=True, timeout=timeout)
             if response.status_code == 200:
-                return True
+                return "Healthy"
             else:
                 time.sleep(2)
-        return False
+        return "Down"
 
     except Exception as e:
         logging.error(e)
-        return False
+        return "Down"
 
 
-def get_deployment_status(deployment_db):
-    deployment_status_data_list = []
-
-    for entry in deployment_db:
-        try:
-            deployment_status = {
-                "Project Name": entry["name"],
-                "Status": check_project_deployment_status(entry["link"], max_retries=3)
-            }
-            deployment_status_data_list.append(deployment_status)
-
-        except Exception as e:
-            logging.error(e)
-            pass
-
-    return deployment_status_data_list
+def get_pageid_from_notion_url(notion_url: str) -> str:
+    return notion_url.split("-")[-1]
 
 
-def get_discord_message_payload(deployment_status_data):
-    payload = ""
-    payload += f"Checked at: {get_current_datetime()}\n\n"
-
-    for entry in deployment_status_data:
-        payload += f"{entry['Project Name']}: {'✅' if entry['Status'] else '❌'}\n"
-
-    return payload
-
-
-def set_logging_config():
-    logging.basicConfig(filename='deployment-tracker.log', level=logging.INFO,
-                        format='%(asctime)s:%(levelname)s:%(message)s')
-
-
-def get_old_new_status(deployments_db_data, deployment_statuses_data):
-    for status in deployment_statuses_data:
-        if status["Status"]:
-            status["Status"] = "Healthy"
-        else:
-            status["Status"] = "Down"
-
-    status_dict = {status["Project Name"]: status for status in deployment_statuses_data}
-
-    changes = []
-
-    for db_data in deployments_db_data:
-        corresponding_status = status_dict.get(db_data["name"])
-
-        if corresponding_status:
-            changes.append({
-                "name": db_data["name"],
-                "old_status": db_data["label"],
-                "new_status": corresponding_status["Status"],
-                "notion_page_id": db_data["notion_page_id"],
-            })
-
-    return changes
-
-
-def update_notion_deployments_database(deployments_data, deployments_data_new):
-    final_statuses_data = get_old_new_status(deployments_data, deployments_data_new)
-
-    for entry in final_statuses_data:
-        if entry["old_status"] != entry["new_status"]:
-            update_status_in_project_page(entry["notion_page_id"], entry["new_status"])
-            logging.info(f"Updated {entry['name']} from {entry['old_status']} to {entry['new_status']}")
-        else:
-            logging.info(f"No Change in {entry['name']}")
-
-
-def update_status_in_project_page(page_id, new_status):
+def update_status_in_project_page(page_id: str, new_status: str) -> int:
     try:
         headers = {
             "Authorization": "Bearer " + NOTION_API_TOKEN,
@@ -196,11 +113,56 @@ def update_status_in_project_page(page_id, new_status):
 
     except Exception as e:
         logging.error(e)
-        return None
+        return -1
 
 
-def get_pageid_from_notion_url(notion_url):
-    return notion_url.split("-")[-1]
+def check_deployment_status_and_update(deployment_data: list[dict]) -> list[dict]:
+    for project in deployment_data:
+        try:
+            latest_status = check_project_deployment_status(project_link=project["link"])
+            project["latest_status"] = latest_status
+
+            if project["latest_status"] != project["status"]:
+                update_status_in_project_page(project["notion_page_id"], latest_status)
+                logging.info(f"Updated {project['name']} from {project['status']} to {latest_status}")
+            else:
+                logging.info(f"No Change in {project['name']}")
+        except Exception as e:
+            logging.error(e)
+
+    return deployment_data
+
+
+def get_discord_message_payload(deployment_status_data: list[dict]) -> str:
+    payload = ""
+    payload += f"Checked at: {get_current_datetime()}\n\n"
+
+    for entry in deployment_status_data:
+        if entry["latest_status"] == "Healthy":
+            payload += f"{entry['name']}: ✅\n"
+        else:
+            payload += f"{entry['name']}: ❌\n"
+
+    return payload
+
+
+def send_message_to_discord(username: str, message: str) -> int:
+    try:
+        headers = {
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "content": message,
+            "username": username,
+        }
+
+        response = requests.post(url=DISCORD_WEBHOOK_URL, data=json.dumps(payload), headers=headers)
+
+        return response.status_code
+
+    except Exception as e:
+        logging.error(e)
+        return -1
 
 
 if __name__ == "__main__":
@@ -212,17 +174,14 @@ if __name__ == "__main__":
         deployment_db_data = extract_data_from_notion_response(deployment_db_raw_data)
         logging.info(f"Deployment DB Data: {deployment_db_data}")
 
-        # Checking Deployment Health Status for each entry in DB
-        deployment_statuses = get_deployment_status(deployment_db_data)
+        # Checking Deployment Health Status and Updating Notion DB
+        deployment_statuses = check_deployment_status_and_update(deployment_db_data)
         logging.info(f"Deployment Statuses: {deployment_statuses}")
 
         # Send Status Report/Alert to Discord
         message_payload = get_discord_message_payload(deployment_statuses)
         send_message_to_discord("Deployments Tracker", message_payload)
         logging.info(f"Message Payload: {message_payload}")
-
-        # Update Notion Database
-        update_notion_deployments_database(deployment_db_data, deployment_statuses)
 
     except Exception as e:
         logging.error(e)
